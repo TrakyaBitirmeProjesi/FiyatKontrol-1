14da84ecaab1a0cbb75f83cb093712d8bba8b54c
anilyilmaz <incelefon@gmail.com>
2018-11-19 15:59:08 +0300

    Value Error

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ def migros_checker(product_name):
     migros_soup = bs4.BeautifulSoup(migros_response.text , "lxml")
     dict = []
     for j in migros_soup.find_all(class_ = "product-card product-action "):
+        try:
+           fiyat = float(j.find(class_="price-tag").text.split("\n")[1].split("T")[0].lstrip().rstrip().replace(",","."))
+        except ValueError:
+            fiyat = j.find(class_="price-tag").text.split("\n")[1].split("T")[0].lstrip().rstrip().replace(",",".")
+
         dict.append({
             "link" : "https://www.sanalmarket.com.tr"+j.find("a")["href"],
             "urun" : j.find(class_="title product-card-title").text,
             "resim" : j.find("img")["src"],
-            "fiyat" : float(j.find(class_="price-tag").text.split("\n")[1].split("T")[0].lstrip().rstrip().replace(",","."))
+            "fiyat" : fiyat
             #eğer istenirse float dönüşümüne uygun
         })
     return dict
@@ -26,11 +31,17 @@ def carrefoursa_checher(product_name):
     carrefoursa_soup = bs4.BeautifulSoup(carrefoursa_response.text , "lxml" )
     dict = []
     for i in carrefoursa_soup.find_all(class_ = "col-xs-6 col-sm-3 col-md-2 col-lg2"):
+        try:
+           fiyat = float(i.find(class_="item-price").text.split("T")[0].replace(",", "."))
+        except ValueError:
+            fiyat = i.find(class_="item-price").text.split("T")[0].replace(",", ".")
+
+
         dict.append({
             "link" : "https://www.carrefoursa.com/tr"+i.find("a")["href"],
             "urun" : i.find(class_ = "item-name").text,
             "resim" : i.find("img")['src'],
-            "fiyat" : float(i.find(class_ = "item-price").text.split("T")[0].replace(",","."))
+            "fiyat" : fiyat
             # eğer istenirse float dönüşümüne uygun
         })
     return dict
